38045d69a4f93e0246a63e807f1ef692740ce09f
Jake Lee <shibas11@gmail.com>
2017-06-11 01:11:03 +0900

    SOLID원칙 OCP - 3. 요구사항 추가 => getTotalArea method가 괴랄하다!(AreaCalculator가 여러 개의 책임을 가지고 있다.)  - 사각형 넒이 계산  - 삼각형 넓이 계산  - 넓이 총합 계산

--- src/shibas11/DesignPattern/ExamRunner.java
+++ src/shibas11/DesignPattern/ExamRunner.java
@@ -9,6 +9,9 @@ import shibas11.DesignPattern.SOLID.OCP.Triangle;
 import shibas11.DesignPattern.SOLID.SRP.Employee;
 import shibas11.DesignPattern.SOLID.SRP.Student;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by shiba on 2017-06-10.
  */
@@ -24,8 +27,14 @@ public class ExamRunner {
                 break;
             case "SOLID.OCP":
                 AreaCalculator ac = new AreaCalculator();
-                System.out.println(String.format("사각형의 넓이는 %f", ac.getArea(new Rectangle(2, 3))));
-                System.out.println(String.format("삼각형의 넓이는 %f", ac.getArea(new Triangle(2, 3))));
+                Rectangle rect = new Rectangle(2, 3);
+                Triangle tri = new Triangle(2, 3);
+                List<Object> list = new LinkedList<Object>();
+                list.add(rect);
+                list.add(tri);
+                System.out.println(String.format("사각형의 넓이는 %f", ac.getArea(rect)));
+                System.out.println(String.format("삼각형의 넓이는 %f", ac.getArea(tri)));
+                System.out.println(String.format("넓이의 총합은 %f", ac.getTotalArea(list)));
                 break;
             case "SOLID.DIP":
                 IAdder adder = AdderFactory.create(AdderFactory.AdderType.X2);
--- src/shibas11/DesignPattern/SOLID/OCP/AreaCalculator.java
+++ src/shibas11/DesignPattern/SOLID/OCP/AreaCalculator.java
@@ -1,5 +1,7 @@
 package shibas11.DesignPattern.SOLID.OCP;
 
+import java.util.List;
+
 /**
  * Created by shiba on 2017-06-11.
  */
@@ -10,5 +12,20 @@ public class AreaCalculator {
 
     public double getArea(Triangle triangle) {
         return triangle.getWidth() * triangle.getHeight() / 2;
+    }
+
+    public double getTotalArea(List<Object> list) {
+        double tot = 0;
+        for (Object obj : list) {
+            if (obj instanceof Rectangle) {
+                Rectangle rect = (Rectangle) obj;
+                tot += this.getArea(rect);
+            } else if (obj instanceof Triangle) {
+                Triangle tri = (Triangle) obj;
+                tot += this.getArea(tri);
+            }
+        }
+
+        return tot;
     }
 }
