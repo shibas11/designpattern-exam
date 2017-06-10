65fb3580e1e81782d6ef8b6a1e9df480437bb88a
Jake Lee <shibas11@gmail.com>
2017-06-10 23:13:17 +0900

    SOLID원칙 DIP - 2. extract interface IAdder

--- src/shibas11/DesignPattern/SOLID/DIP/Calculator.java
+++ src/shibas11/DesignPattern/SOLID/DIP/Calculator.java
@@ -4,7 +4,7 @@ package shibas11.DesignPattern.SOLID.DIP;
  * Created by shiba on 2017-06-10.
  */
 public class Calculator {
-    private Adder adder;
+    private IAdder adder;
 
     public Calculator() {
         this.adder = new Adder();
---
+++ src/shibas11/DesignPattern/SOLID/DIP/IAdder.java
@@ -0,0 +1,8 @@
+package shibas11.DesignPattern.SOLID.DIP;
+
+/**
+ * Created by shiba on 2017-06-10.
+ */
+public interface IAdder {
+    double add(double d1, double d2);
+}
