f51dd18064e2081432edb24fdd63d3e085af7ea5
Jake Lee <shibas11@gmail.com>
2017-06-10 23:06:09 +0900

    add class ExamRunner

---
+++ src/shibas11/DesignPattern/ExamRunner.java
@@ -0,0 +1,15 @@
+package shibas11.DesignPattern;
+
+/**
+ * Created by shiba on 2017-06-10.
+ */
+public class ExamRunner {
+    public void run(String code) {
+        System.out.println("=== " + code + " ===");
+        switch(code) {
+            default:
+                break;
+        }
+        System.out.println();
+    }
+}
--- src/shibas11/Main.java
+++ src/shibas11/Main.java
@@ -1,7 +1,11 @@
 package shibas11;
 
+import shibas11.DesignPattern.ExamRunner;
+
 public class Main {
 
     public static void main(String[] args) {
+        ExamRunner runner = new ExamRunner();
+        runner.run("SOLID.DIP");
     }
 }
