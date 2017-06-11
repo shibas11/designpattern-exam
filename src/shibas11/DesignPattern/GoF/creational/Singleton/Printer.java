30178d59c962fdfef24d0702d96c97c54bc4bedb
Jake Lee <shibas11@gmail.com>
2017-06-11 23:17:07 +0900

    creational.Singleton pattern - 4. 해결책 1번. 정적 변수 바로 초기값 주기. 하지만 상태 변수는 여전히 이상하다.

--- src/shibas11/DesignPattern/GoF/creational/Singleton/Printer.java
+++ src/shibas11/DesignPattern/GoF/creational/Singleton/Printer.java
@@ -4,26 +4,26 @@ package shibas11.DesignPattern.GoF.creational.Singleton;
  * Created by shiba on 2017-06-11.
  */
 public class Printer {
-    private static Printer printer;
+    private static Printer printer = new Printer();
     private int counter = 0;
 
     private Printer() {
     }
 
     public static Printer getPrinter() {
-        if (printer == null) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-            }
-            printer = new Printer();
-        }
+//        if (printer == null) {
+//            try {
+//                Thread.sleep(1);
+//            } catch (InterruptedException e) {
+//            }
+//            printer = new Printer();
+//        }
         return printer;
     }
 
     public void print(String str) {
         counter++;
-        System.out.println(str + "counter= " + counter);
+        System.out.println(str + " counter= " + counter);
     }
 
 }
