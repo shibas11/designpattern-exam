bc764cbbe26700f5f7a57de2630492cc781a624f
Jake Lee <shibas11@gmail.com>
2017-06-11 18:06:23 +0900

    SOLID원칙 ISP - 1. ISP 위반: 펭귄은 날지 못한다!(Bird interface가 잘못)

--- src/shibas11/Main.java
+++ src/shibas11/Main.java
@@ -8,7 +8,8 @@ public class Main {
         ExamRunner runner = new ExamRunner();
         runner.run("SOLID.SRP"); // Single Responsibility   책임 분리: 클래스는 한 개의 책임만 가져라
         runner.run("SOLID.OCP"); // Open-Closed             개방-폐쇄: 기존 코드 변경없이 새 기능 추가해라, UnitTest!!
-        runner.run("SOLID.LSP"); // Liskov Substitution     리스코프 치환: 부모,자식 클래스 사이의 행위가 일관성이 있어야 한다.
+        runner.run("SOLID.LSP"); // Liskov Substitution     리스코프 치환: 부모,자식 클래스 사이의 행위의 일관성
         runner.run("SOLID.DIP"); // Dependency Inversion    의존 역전: 변하지 않거나 변하기 어려운 것(추상)에 의존하라
+        runner.run("SOLID.ISP"); // Interface Segregation   인터페이스 분리: 클라이언트에 특화되도록 인터페이스 분리해라
     }
 }
