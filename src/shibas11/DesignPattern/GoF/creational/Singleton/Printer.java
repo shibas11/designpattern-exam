76d6cdffaab994341c30173ee1e6eebc87e91132
Jake Lee <shibas11@gmail.com>
2017-06-11 23:21:31 +0900

    creational.Singleton pattern - 4. 해결책 2번. synchronized 사용하여 동기화 하지만 상태 변수는 여전히 이상하다.

--- src/shibas11/DesignPattern/GoF/creational/Singleton/Printer.java
+++ src/shibas11/DesignPattern/GoF/creational/Singleton/Printer.java
@@ -4,26 +4,28 @@ package shibas11.DesignPattern.GoF.creational.Singleton;
  * Created by shiba on 2017-06-11.
  */
 public class Printer {
-    private static Printer printer = new Printer();
+    private static Printer printer = null;
     private int counter = 0;
 
     private Printer() {
     }
 
-    public static Printer getPrinter() {
-//        if (printer == null) {
-//            try {
-//                Thread.sleep(1);
-//            } catch (InterruptedException e) {
-//            }
-//            printer = new Printer();
-//        }
+    public synchronized static Printer getPrinter() { // 메서드 동기화
+        if (printer == null) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+            }
+            printer = new Printer();
+        }
         return printer;
     }
 
     public void print(String str) {
-        counter++;
-        System.out.println(str + " counter= " + counter);
+        synchronized (this) { // 오직 하나의 스레드만 접근을 허용함
+            counter++;
+            System.out.println(str + " counter= " + counter);
+        }
     }
 
 }
