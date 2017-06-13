77e6b634ee7807851de8bbfd48b9ec8e2177e147
Jake Lee <shibas11@gmail.com>
2017-06-14 00:04:51 +0900

    behavioral.State pattern - 1. add Light class(ON, OFF 상태만 존재)

--- src/shibas11/Main.java
+++ src/shibas11/Main.java
@@ -17,13 +17,18 @@ public class Main {
         전략(변화하는 것)을 찾아서 클래스로 캡슐화한다.
         필요할 때마다 교체할 수 있도록.
         */
-        runner.run("Strategy");
+        //runner.run("Strategy");
 
         /*
         생성.싱글턴 패턴 creational.Singleton
         전역변수 사용없이 객체를 하나만 생성하여
         어디에서든지 참조할 수 있게 함.
         */
-        runner.run("Singleton");
+        //runner.run("Singleton");
+
+        /*
+        행위.스테이트 패턴 behavioral.State
+         */
+        runner.run("State");
     }
 }
