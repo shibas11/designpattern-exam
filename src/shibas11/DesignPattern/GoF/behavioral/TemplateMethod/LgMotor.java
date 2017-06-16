6e146273832864f18df282374842925fc0e66d04
Jake <kc80.lee@samsung.com>
2017-06-16 11:52:01 +0900

    behavioral.TemplateMethod - 2. add class LGMotor: 코드 중복 일어남

---
+++ src/shibas11/DesignPattern/GoF/behavioral/TemplateMethod/LgMotor.java
@@ -0,0 +1,40 @@
+package shibas11.DesignPattern.GoF.behavioral.TemplateMethod;
+
+/**
+ * Created by SDS on 2017-06-16.
+ */
+public class LGMotor {
+    private Door door;
+    private MotorStatus motorStatus;
+
+    public LGMotor(Door door) {
+        this.door = door;
+        motorStatus = MotorStatus.STOPPED;
+    }
+
+    public void move(Direction direction) {
+        MotorStatus motorStatus = getMotorStatus();
+        if (motorStatus == MotorStatus.MOVING) // 이미 이동 중이면 아무 작업을 하지 않음
+            return;
+
+        DoorStatus doorStatus = door.getDoorStatus();
+        if (doorStatus == DoorStatus.OPENED) // 문이 열려있으면 우선 문을 닫음
+            door.close();
+
+        moveLGMotor(direction); // 모터 구동
+        setMotorStatus(MotorStatus.MOVING); // 모터 상태를 이동 중으로 변경
+    }
+
+    private void moveLGMotor(Direction direction) {
+        // LG Motor를 구동시킴
+        System.out.println(this.getClass().getSimpleName() + " 구동시킴");
+    }
+
+    public MotorStatus getMotorStatus() {
+        return motorStatus;
+    }
+
+    public void setMotorStatus(MotorStatus motorStatus) {
+        this.motorStatus = motorStatus;
+    }
+}
